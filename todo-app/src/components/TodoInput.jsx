4e48c7a0ff693ab319fcd2744a96450fd3e4f7d0
daniel <danimolinasantos@gmail.com>
2025-07-12 21:47:02 -0700

    Added the Enter key functionallity for the TodoInput

--- todo-app/src/components/TodoInput.jsx
+++ todo-app/src/components/TodoInput.jsx
@@ -5,15 +5,27 @@ export function TodoInput(props) {
     const [inputValue, setInputValue] = useState("")
     console.log(inputValue)
 
+    const handleSubmit = () => {
+        if(!inputValue) { return }
+        handleAddTodo(inputValue)
+        setInputValue("")
+    }
+
+    const handleKeyDown = (e) => {
+        if(e.key === 'Enter') {
+            handleSubmit()
+        }
+    }
+
     return (
         <div className="input-container">
-            <input value={inputValue} onChange={(e) => {setInputValue(e.target.value)}} 
-            placeholder = "Add a new task..." />
-            <button onClick={() => {
-                if(!inputValue) { return }
-                handleAddTodo(inputValue)
-                setInputValue("")
-            }}>
+            <input 
+                value={inputValue} 
+                onChange={(e) => {setInputValue(e.target.value)}} 
+                onKeyDown={handleKeyDown}
+                placeholder="Add a new task..." 
+            />
+            <button onClick={handleSubmit}>
                 <i className="fa-solid fa-plus"></i>
             </button>
         </div>
